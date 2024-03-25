b242bd048cbe08e75947d3ec610b10fc92c7c385
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-03-25 14:43:15 +0700

    masukkan int pilihan

--- 098_FungsiSwitch/098_FungsiSwitch.cpp
+++ 098_FungsiSwitch/098_FungsiSwitch.cpp
@@ -28,7 +28,26 @@ float perkalian(float a, float b)
 {
     return a + b;
 }
+int main()
+{
+    int pilihan;
 
+    do
+    {
+        cout << "==" << endl;
+        cout << "MENU" << endl;
+        cout << "1. penjumlahan " << endl;
+        cout << "2. pembagian " << endl;
+        cout << "3. perkalian " << endl;
+        cout << "4. pengurangan " << endl;
+        cout << "5. EXIT " << endl;
+        cout << "Masukkan pilihan 1/2/3/4/5 :";
+        cin >> pilihan;         
+    }
+    
+
+
+}
 
 
 int main()
