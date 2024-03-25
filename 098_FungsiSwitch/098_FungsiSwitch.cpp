1b2ba307982ba151788fb78b854a526827952456
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-03-25 14:34:11 +0700

    masukkan float perkalian

--- 098_FungsiSwitch/098_FungsiSwitch.cpp
+++ 098_FungsiSwitch/098_FungsiSwitch.cpp
@@ -4,6 +4,28 @@ using namespace std;
 
 int bilSatu, bilDua;
 
+void input()
+{
+    cout << "Masukkan Bilangan Pertama = ";
+    cin >> bilSatu;
+    cout << "Masukkan Bilangan Kedua = ";
+    cin >> bilDua;
+}
+
+float penjumlahan(float a, float b)
+{
+    return a + b;
+}
+float pembagian(float a, float b)
+{
+    return a + b;
+}
+float perkalian(float a, float b)
+{
+    return a + b;
+}
+
+
 
 
 int main()
