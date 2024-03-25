f0416cf0dd388dbaf26dfd68f1c26f64f55adbf5
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-03-25 14:27:08 +0700

    masukkan bilangan

--- 098_FungsiSwitch/098_FungsiSwitch.cpp
+++ 098_FungsiSwitch/098_FungsiSwitch.cpp
@@ -4,13 +4,7 @@ using namespace std;
 
 int bilSatu, bilDua;
 
-void input()
-{
-    cout << "Masukkan Bilangan Pertama = ";
-    cin >> bilSatu;
-    cout << "Masukkan Bilangan Kedua = ";
-    cin >> bilDua;
-}
+
 
 int main()
 {
